40f61ddfe6a8c576d1f7c6e0993da79e268f4976
Robert Peterman <robpeterman573@gmail.com>
2023-05-07 10:12:32 -0400

    set ssl rejectUnauthorized to false

--- config/database.js
+++ config/database.js
@@ -61,7 +61,7 @@ module.exports = ({ env }) => {
         database: env("DATABASE_NAME", "strapi"),
         user: env("DATABASE_USERNAME", "strapi"),
         password: env("DATABASE_PASSWORD", "strapi"),
-        ssl: false,
+        ssl: { rejectUnauthorized: false },
         schema: env("DATABASE_SCHEMA", "public"),
       },
       pool: {
